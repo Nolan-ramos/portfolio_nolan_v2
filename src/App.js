0c0be9ce13c4787573074dc0ae3209f56d6787d3
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-03 10:37:39 +0100

    push font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.scss';
+import './styles/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <p className="text">Coucou</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p className="text">Coucou</p>
+        </div>
+    );
 }
 
 export default App;
