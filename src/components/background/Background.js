1ea56e1be68a747623bfd0fe2428b136c1148000
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-10 17:35:04 +0100

    problem netlify

---
+++ src/components/background/Background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Background = () => {
+    return (
+        <div className='background'>
+            <div className='background__shapes'>
+                <div className='background__shape background__shape__first'></div>
+                <div className='background__shape background__shape__second'></div>
+            </div>
+        </div>
+    );
+};
+
+export default Background;
