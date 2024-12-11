c9204141fa2731626a46492595a1292f485c3273
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-11 20:07:45 +0100

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div className='about'>
-            
+            <h1 className='text-hover mouse-hover'>About</h1>
+            <p>Bienvenue sur la Aboutpage</p>
         </div>
     );
 };
