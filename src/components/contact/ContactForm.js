7664ffedfed6f3cb72c5af90d43d7a7280a37fad
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-13 18:18:56 +0100

    test form netlify

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -5,14 +5,12 @@ const ContactForm = () => {
     const [isSubmitted, setIsSubmitted] = useState(false);
 
     const handleSubmit = (event) => {
-        event.preventDefault(); // Empêche la redirection
+        event.preventDefault();
 
         setIsSubmitting(true);
 
-        // Crée un objet FormData à partir du formulaire
         const formData = new FormData(event.target);
 
-        // Envoi des données via une requête fetch (optionnel)
         fetch('/', {
             method: 'POST',
             headers: { 'Accept': 'application/json' },
@@ -20,14 +18,13 @@ const ContactForm = () => {
         })
             .then((response) => {
                 if (response.ok) {
-                    setIsSubmitted(true);
-                    alert('Thank you! Your form has been submitted.');
+                    setIsSubmitted(true); 
                 } else {
-                    alert('Something went wrong. Please try again.');
+                    console.log('Something went wrong. Please try again.');
                 }
             })
             .catch((error) => {
-                alert('An error occurred. Please try again.');
+                console.log('An error occurred. Please try again.');
             })
             .finally(() => {
                 setIsSubmitting(false);
@@ -36,41 +33,41 @@ const ContactForm = () => {
 
     return (
         <div className="contact__container__form__content">
-            <form
-                name="contact"
-                method="POST"
-                data-netlify="true"
-                onSubmit={handleSubmit}
-            >
-                <input type="hidden" name="form-name" value="contact" />
-                <label htmlFor="name">Name</label> <br />
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    required
-                />
-                <label htmlFor="email">Email</label> <br />
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    required
-                />
-                <label htmlFor="message">Message</label> <br />
-                <textarea
-                    id="message"
-                    name="message"
-                    required
-                ></textarea>
-                <input
-                    type="submit"
-                    value={isSubmitting ? 'Submitting...' : 'Submit message'}
-                    disabled={isSubmitting}
-                />
-            </form>
-
-            {isSubmitted && !isSubmitting && (
+            {!isSubmitted ? (
+                <form
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    onSubmit={handleSubmit}
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+                    <label htmlFor="name">Name</label> <br />
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        required
+                    />
+                    <label htmlFor="email">Email</label> <br />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                    />
+                    <label htmlFor="message">Message</label> <br />
+                    <textarea
+                        id="message"
+                        name="message"
+                        required
+                    ></textarea>
+                    <input
+                        type="submit"
+                        value={isSubmitting ? 'Submitting...' : 'Submit message'}
+                        disabled={isSubmitting}
+                    />
+                </form>
+            ) : (
                 <p className="form-success-message">Thank you for your submission!</p>
             )}
         </div>
