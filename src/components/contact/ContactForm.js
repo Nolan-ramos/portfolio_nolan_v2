b83f138128232942c245283399dddae3d693caa7
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-13 18:01:28 +0100

    test form netlify

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,43 +1,20 @@
-// src/components/ContactForm.js
-import React, { useState } from 'react';
+import React from 'react';
 
 const ContactForm = () => {
-  const [name, setName] = useState(''); // État pour le champ "name"
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Gestion de la soumission si nécessaire
-    console.log('Form submitted with name:', name);
-  };
-
-  return (
-    <div className="contact__container__form__content">
-      <form
-        name="contact" // Nom du formulaire
-        method="POST" // Méthode de soumission
-        data-netlify="true" // Attribut nécessaire pour que Netlify gère le formulaire
-        onSubmit={handleSubmit} // Appel de la fonction handleSubmit lors de la soumission
-      >
-        {/* Champ caché pour Netlify avec le nom du formulaire */}
-        <input type="hidden" name="form-name" value="contact" />
-
-        {/* Champ de saisie avec un seul champ de nom */}
-        <label>
-          Your Name:
-          <input
-            type="text"
-            name="name" // Nom du champ, correspond à l'attribut name dans le formulaire Netlify
-            value={name} // Valeur de l'input liée à l'état React
-            onChange={(e) => setName(e.target.value)} // Mise à jour de l'état lors de la saisie
-            required // Le champ est obligatoire
-          />
-        </label>
-
-        {/* Bouton de soumission */}
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className='contact__container__form__content'>
+            <form name="contact" method="post">
+                <input type="hidden" name="form-name" value="contact" />
+                <label htmlFor="name">Name</label> <br />
+                <input type="text" id="name" name="name" required />
+                <label htmlFor="email">Email</label> <br />
+                <input type="email" id="email" name="email" required />
+                <label htmlFor="message">Message</label> <br />
+                <textarea id="message" name="message" required></textarea>
+                <input type="submit" value="Submit message" />
+            </form>
+        </div>
+    );
 };
 
 export default ContactForm;
