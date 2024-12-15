408007df06be621f03aed31dd471f83c347239fb
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-15 14:23:37 +0100

    push projects page

--- src/components/projects/ProjectsBar.js
+++ src/components/projects/ProjectsBar.js
@@ -7,19 +7,25 @@ const ProjectsBar = () => {
     const toggleProjects = () => {
         setIsProjectsOpen(!isProjectsOpen);
     };
+
+    // Liste des langages
+    const languages = ['Vue.js', 'React.js', 'Express.js', 'JavaScript'];
+
     return (
         <div className='projects__bar'>
             <div className={`projects__bar__collapse
                 ${isProjectsOpen ? 'projects__bar__collapse__open' : ''}`}>
-                <div className='projects__bar__collapse__title text-hover mouse-hover' onClick={toggleProjects}>
-                    <MaterialSymbolsArrowDropDown/>
+                <div 
+                    className='projects__bar__collapse__title text-hover mouse-hover' 
+                    onClick={toggleProjects}
+                >
+                    <MaterialSymbolsArrowDropDown />
                     <span>Projets</span>
                 </div>
                 <div className="projects__bar__collapse__languages">
-                    <div>Vue.js</div>
-                    <div>React.js</div>
-                    <div>Express.js</div>
-                    <div>JavaScript</div>
+                    {languages.map((language) => (
+                        <ProjectsBarLanguage key={language} language={language} />
+                    ))}
                 </div>
             </div>
         </div>
---
+++ src/components/projects/ProjectsBarLanguage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsBarLanguage = ({ language }) => {
+    return (
+        <div className='projects__bar__collapse__languages__language'>
+            {language}
+        </div>
+    );
+};
+
+export default ProjectsBarLanguage;
