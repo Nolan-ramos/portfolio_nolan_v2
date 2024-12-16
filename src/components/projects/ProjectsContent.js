fdf70334eda02e7cdb10943843cb25cbe06e2440
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-16 21:17:58 +0100

    push project page

--- src/components/projects/ProjectsContent.js
+++ src/components/projects/ProjectsContent.js
@@ -1,5 +1,6 @@
 import project1 from './img/project1.png';
 import project2 from './img/project2.png';
+import project3 from './img/project3.png';
 
 const ProjectsContent = () => {
     return (
@@ -12,7 +13,7 @@ const ProjectsContent = () => {
                         <div className='projects__container__content__project__infos__container'>
                             <p className='projects__container__content__project__infos__container__text'>
                                 <span>Ce projet est la première version de mon portfolio.</span>
-                                <span>Ces informations sont fictives et servent uniquement à illustrer le design.</span>
+                                <span>Les informations sont fictives et servent uniquement à illustrer le design.</span>
                             </p>
                             <a 
                                 className='projects__container__content__project__infos__container__link mouse-hover' 
@@ -27,11 +28,11 @@ const ProjectsContent = () => {
                 <div className='projects__container__content__project'>
                     <h3 className='projects__container__content__project__title'>Projet 2</h3>
                     <div className='projects__container__content__project__infos'>
-                        <img className="projects__container__content__project__infos__image" src={project2} alt="project1"></img>
+                        <img className="projects__container__content__project__infos__image" src={project2} alt="project2"></img>
                         <div className='projects__container__content__project__infos__container'>
                             <p className='projects__container__content__project__infos__container__text'>
-                                <span>Ce projet est la première version de mon portfolio.</span>
-                                <span>Ces informations sont fictives et servent uniquement à illustrer le design.</span>
+                                <span>Développement du site d'une MARPA où travaille une connaissances.</span>
+                                <span>L'objectif était de faire une refonte du site existant, en le modernisant.</span>
                             </p>
                             <a 
                                 className='projects__container__content__project__infos__container__link mouse-hover' 
@@ -44,57 +45,19 @@ const ProjectsContent = () => {
                     </div>
                 </div>
                 <div className='projects__container__content__project'>
-                    <h3 className='projects__container__content__project__title'>Projet 2</h3>
+                    <h3 className='projects__container__content__project__title'>Projet 3</h3>
                     <div className='projects__container__content__project__infos'>
-                        <img className="projects__container__content__project__infos__image" src={project2} alt="project1"></img>
+                        <img className="projects__container__content__project__infos__image" src={project3} alt="project3"></img>
                         <div className='projects__container__content__project__infos__container'>
                             <p className='projects__container__content__project__infos__container__text'>
-                                <span>Ce projet est la première version de mon portfolio.</span>
-                                <span>Ces informations sont fictives et servent uniquement à illustrer le design.</span>
+                                <span>Développement d'un portfolio pour un ami.</span>
+                                <span>Les informations sont fictives et servent uniquement à illustrer le design.</span>
                             </p>
                             <a 
                                 className='projects__container__content__project__infos__container__link mouse-hover' 
                                 rel="noopener noreferrer"
                                 target="_blank"
-                                href="https://marpa.vercel.app/index.html">
-                                Voir le projet
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className='projects__container__content__project'>
-                    <h3 className='projects__container__content__project__title'>Projet 1</h3>
-                    <div className='projects__container__content__project__infos'>
-                        <img className="projects__container__content__project__infos__image" src={project1} alt="project1"></img>
-                        <div className='projects__container__content__project__infos__container'>
-                            <p className='projects__container__content__project__infos__container__text'>
-                                <span>Ce projet est la première version de mon portfolio.</span>
-                                <span>Ces informations sont fictives et servent uniquement à illustrer le design.</span>
-                            </p>
-                            <a 
-                                className='projects__container__content__project__infos__container__link mouse-hover' 
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://nolan.3ntf.fr/">
-                                Voir le projet
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className='projects__container__content__project'>
-                    <h3 className='projects__container__content__project__title'>Projet 1</h3>
-                    <div className='projects__container__content__project__infos'>
-                        <img className="projects__container__content__project__infos__image" src={project1} alt="project1"></img>
-                        <div className='projects__container__content__project__infos__container'>
-                            <p className='projects__container__content__project__infos__container__text'>
-                                <span>Ce projet est la première version de mon portfolio.</span>
-                                <span>Ces informations sont fictives et servent uniquement à illustrer le design.</span>
-                            </p>
-                            <a 
-                                className='projects__container__content__project__infos__container__link mouse-hover' 
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://nolan.3ntf.fr/">
+                                href="https://portfolio-ami.vercel.app/">
                                 Voir le projet
                             </a>
                         </div>
