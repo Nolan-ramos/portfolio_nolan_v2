067e8a81064a128bd1b7e9afcc3e71ee9f59d3c5
Nolan-ramos <nolan.ramos.01@gmail.com>
2024-12-21 19:52:13 +0100

    push line page about

--- src/components/about/AboutText.js
+++ src/components/about/AboutText.js
@@ -4,17 +4,27 @@ const AboutText = ({ activeCategorie }) => {
     return (
         <div className='about__container__infos__content__text'>
             <div className='about__container__infos__content__text__center'>
+                <div className='about__container__infos__content__text__center__line'>
+                    {Array.from({ length: 50 }).map((_, index) => (
+                        <div key={index} className='number-line'>
+                            <span>{index + 1}</span>
+                            <span>*</span>
+                        </div>
+                    ))}
+                </div>
+                <div className='about__container__infos__content__text__center__content'>
                 {Array.isArray(activeCategorie?.text) ? (
                     activeCategorie.text.map((line, index) => (
                         <p key={index}>
                             {line}
-                            {activeCategorie?.section === 'Compétences' && index < activeCategorie.text.length - 1 && ' -'}
-                            {activeCategorie?.key === 'interets' && index < activeCategorie.text.length - 1 && ' -'}
+                            {activeCategorie?.section === 'Compétences' && index < activeCategorie.text.length - 1 && ' - '}
+                            {activeCategorie?.key === 'interets' && index < activeCategorie.text.length - 1 && ' - '}
                         </p>
                     ))
                 ) : (
                     <p>{activeCategorie?.text}</p>
                 )}
+                </div>
             </div>
         </div>
     );
